2d65206ae5b0b626d6890d1464ebb018bbcae50f
Shiwani <shiwaniverma2060@gmail.com>
2026-06-02 12:22:47 +0530

    feat: update API URL for production

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 import axios from "axios";
-
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
 
 interface SearchResult {
   id: string;
